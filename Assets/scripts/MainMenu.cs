a16b7973200fc6728650dab44a4628200f794192
Steven Andrew Green <s.a.green51@gmail.com>
2018-12-13 15:38:19 -0800

    begining the menu and other screens

---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void LoadingMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Instructions()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
